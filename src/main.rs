cfa5a0f05dbd5386e59049ab0ca4b17834fa4cbf
pchchv <ipchchv@gmail.com>
2022-12-03 10:23:50 +0300

    Implement a user input

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::io;
 
-use std::io;
-
 fn main() {
     println!("You can enter 'exit' anytime to quit.");
 
-    let mut cc_number = String::new();
-
     loop {
-        println!("Please enter a CC number to validate: ")
+        println!("Please enter a CC number to validate: ");
 
-        io::stdin().read_line(&mut cc_number).expect("Failed to read line");
+        let mut cc_number = String::new();
+
+        io::stdin()
+            .read_line(&mut cc_number)
+            .expect("Failed to read line");
+
+        cc_number = cc_number.trim().to_string();
+
+        if cc_number == "exit" {
+            break;
+        }
+
+        println!("{cc_number}");
     }
 }
