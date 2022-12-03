8e78972f0030377671fd18eb85ca145d81451266
pchchv <ipchchv@gmail.com>
2022-12-03 11:04:16 +0300

    Implement a parsing str to vec

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use std::io;
 
+fn validator(cc_number: Vec<u8>) -> bool {
+    println!("{:?}", cc_number);
+
+    false
+}
+
+fn to_vec(ccn: &str) -> Vec<u8> {
+    let ccn = ccn.split("");
+    let mut cc_number: Vec<u8> = Vec::new();
+
+    for i in ccn {
+        println!("{i}");
+        let i: u8 = match i.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        cc_number.push(i);
+    }
+
+    cc_number
+}
+
 fn main() {
     println!("You can enter 'exit' anytime to quit.");
 
@@ -12,14 +35,17 @@ fn main() {
             .read_line(&mut cc_number)
             .expect("Failed to read line");
 
-        cc_number = cc_number.trim().to_string();
+        let cc_number = cc_number.trim();
 
         if cc_number == "exit" {
             break;
         }
 
         if cc_number.parse::<u128>().is_ok() {
-            println!("{cc_number}");
+            let cc_number = to_vec(cc_number);
+            println!("{}", validator(cc_number))
+        } else {
+            eprintln!("Enter the correct number (numbers only)")
         }
     }
 }
